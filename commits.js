20fd3033cb7a772a4945b96692b436cb290d6fa3
Samuel <58521841+IronHead43@users.noreply.github.com>
2022-06-11 14:50:57 +0100

    Updated total commits section

--- commits.js
+++ commits.js
@@ -1,13 +1,26 @@
-function outputTotalCommits() {
+function csharpCommits() {
     parseText=String(this.responseText);
     i=19;
     while (parseText.slice(i,i+1) != ","){
         i+=1;
     }
-    parseText=parseText.slice(19,i)
-    document.getElementById("totalCommits").innerHTML = parseText + " contributions in last year";
+    parseText=parseText.slice(19,i);
+    document.getElementById("totalCommitsCSharp").innerHTML = parseText + " contributions in last year";
+  }
+function pythonCommits() {
+    parseText=String(this.responseText);
+    i=19;
+    while (parseText.slice(i,i+1) != ","){
+        i+=1;
+    }
+    parseText=parseText.slice(19,i);
+    document.getElementById("totalCommitsPython").innerHTML = parseText + " contributions in last year";
   }
   var request = new XMLHttpRequest();
-  request.onload = outputTotalCommits;
+  request.onload = pythonCommits;
   request.open('get', 'https://api.github.com/repos/IronHead43/python/stats/contributors', true)
+  request.send()
+  var request = new XMLHttpRequest();
+  request.onload = csharpCommits;
+  request.open('get', 'https://api.github.com/repos/IronHead43/CSharp/stats/contributors', true)
   request.send()
